1dcc0450ebe093484cb33b4406db9f4581c43db1
w0tn <w0tn@pm.me>
2026-07-04 18:26:09 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'wotn-weather-v1';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/manifest.json'
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.json'
 ];
 
 self.addEventListener('install', (evt) => {
